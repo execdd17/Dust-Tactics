c632ea1b6a143b132025c59b411ac1c3f6184262
Alexander Vanadio <execdd17@gmail.com>
2012-02-27 08:52:16 -0500

    Added a few more tests for Rhino

--- spec/units/rhino_spec.rb
+++ spec/units/rhino_spec.rb
@@ -9,4 +9,16 @@ describe DustTactics::Units::Rhino do
   it "should have two weapon lines" do
     @rhino.weapon_lines.length.should == 2
   end
+
+  it "should have 4 hit points" do
+    @rhino.hit_points.should == 4
+  end
+
+  it "should be type infantry" do
+    @rhino.type.should == :infantry
+  end
+
+  it "should have an armor rating of 3" do
+    @rhino.armor.should == 3
+  end
 end
