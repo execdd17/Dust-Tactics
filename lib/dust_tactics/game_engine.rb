ccb4095e2db6d36e407e53773acad2d4bb5a2b6d
Alexander Vanadio <execdd17@gmail.com>
2012-03-03 04:54:48 -0500

    Added GameEngine.initiative and specs

--- lib/dust_tactics/game_engine.rb
+++ lib/dust_tactics/game_engine.rb
@@ -1,4 +1,14 @@
 module DustTactics
   class GameEngine
+
+    # Used to determine who gets to place cover first and who starts the round
+    def self.initiative(player_one, player_two)
+      begin
+        p1_results = DiceEngine.roll(3)  
+        p2_results = DiceEngine.roll(3)
+      end while p1_results[:hits] == p2_results[:hits]  #ties are lame
+
+      p1_results[:hits] > p2_results[:hits] ? player_one : player_two
+    end
   end
 end
--- spec/game_engine_spec.rb
+++ spec/game_engine_spec.rb
@@ -3,11 +3,17 @@ require 'spec_helper'
 describe DustTactics::GameEngine do
 
   before(:each) do
-    @game_engine = GameEngine.new
+    board             = Board.new(BOARD_ROWS, BOARD_COLUMNS)
+    @allied_player    = Player.new("Franky Four Fingers", "allied", board)
+    @axis_player      = Player.new("Johnny English"     , "axis",   board)
   end
 
-  it "should calculate who gets to place cover first" do
-    pending
+  context ".initiative" do
+    it "should calculate who wins the initiative" do
+      [@allied_player, @axis_player].include?( 
+        GameEngine.initiative(@allied_player, @axis_player)).should == true
+    end
   end
+
 
 end
