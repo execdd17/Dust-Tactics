deb80db54a93cc54f5b32244923782aeddc56d4c
Alexander Vanadio <execdd17@gmail.com>
2012-03-25 02:07:28 -0400

    Implemented FSM as part of Player with many specs; filtered out specs from simplecov

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,7 +5,7 @@ describe DustTactics::Player do
   before(:each) do
     board   = Board.new(BOARD_ROWS, BOARD_COLUMNS)
     @player = Player.new("Franky Four Fingers", "axis", board)
-    @unit   = [Units::Rhino.new, Units::Lara.new].sample
+    @unit   = rand_interactable_unit
   end
 
   it "should have a name" do
@@ -59,26 +59,125 @@ describe DustTactics::Player do
   end
 
   it "should raise an exception when trying to move a unit it doesn't own" do
+    lambda { @player.move_unit(@unit, nil) }.should raise_error BusyHands
+  end
+      
+  it "should raise an exception when attempting to move a unit without " << 
+     "enough ticks" do
+    
+    start_space = @player.board.rand_space
+    end_space   = @player.board.rand_space( [start_space] )
+    
+    @player.ticks= 0
+    @player.add_unit(@unit)
+    @unit.deploy(start_space)
+    
+    lambda do
+      @player.move_unit(@unit, end_space)
+    end.should raise_error BigSpender, "Not Enough Ticks!"
+  end
+
+  it "should raise an exception when attempting to attack a unit without " << 
+     "enough ticks" do
+    
+    start_space = @player.board.rand_space
+    end_space   = @player.board.rand_space( [start_space] )
+    
+    @player.ticks= 0
+    @player.add_unit(@unit)
+    @unit.deploy(start_space)
+    
+    lambda do
+      @player.attack_unit(nil,nil,nil)
+    end.should raise_error BigSpender, "Not Enough Ticks!"
+  end
+
+  it "should raise an exception when attempting to skip an action without " << 
+     "enough ticks" do
+    
+    @player.ticks= 0
+    
+    lambda do
+      @player.skip_action
+    end.should raise_error BigSpender, "Not Enough Ticks!"
+  end
+  
+  it "should allow the option to skip an action" do
+    lambda { @player.skip_action }.should_not raise_error
+  end
+
+  it "should allow the option to attack a unit" do
+    attacker, defender = deployment_factory(@board, :close_combat)
+    lambda do 
+      @player.attack_unit(attacker, defender, attacker.weapon_lines)
+    end.should_not raise_error
+  end
+
+  it "should remove the defeated unit from the oponent's units array" do
     pending
   end
-    
-  #TODO: toss this an use a FSM instead! Make it an instance variable of player
-  describe "#perform_turn" do
 
-    before(:each) do
-      board   = Board.new(BOARD_ROWS, BOARD_COLUMNS)
-      @player = Player.new("Franky Four Fingers", "axis", board)
-      @attacker, @defender = rand_interactable_unit, rand_interactable_unit
+  describe "a turn finate state machine" do
+    describe "possible transitions" do
+
+      before(:each) { @player = Player.new nil, nil, nil }
+
+      it "should allow the transition from start to performed_sustained attack" do
+        @player.can_sustained_attack?.should == true
+      end
+
+      it "should deduct the correct amount of ticks for a given action" do
+        starting_ticks = @player.ticks= 3
+        @player.sustained_attack
+        @player.ticks.should == starting_ticks - @player.tick_cost
+      end
+
+      it "should allow the transition from start to attacked" do
+        @player.can_attack?.should == true
+      end
+
+      it "should allow the transition from start to moved" do
+        @player.can_move?.should == true
+      end
+
+      it "should allow the transition from start to did_nothing" do
+        @player.can_do_nothing?.should == true
+      end
+
+      it "should allow the transition from did_nothing to did_nothing" do
+        @player.do_nothing
+        @player.can_do_nothing?.should == true
+      end
+
+      it "should allow the transition from did_nothing to moved" do
+        @player.do_nothing
+        @player.can_move?.should == true
+      end
+
+      it "should allow the transition from did_nothing attacked" do
+        @player.do_nothing
+        @player.can_attack?.should == true
+      end
+
+      it "should allow the transition from attacked to did_nothing" do
+        @player.attack
+        @player.can_do_nothing?.should == true
+      end
+
+      it "should allow the transition from moved to did_nothing" do
+        @player.move
+        @player.can_do_nothing?.should == true
+      end
+
+      it "should allow the transition from moved to moved" do
+        @player.move
+        @player.can_move?.should == true
+      end
+      
+      it "should allow the transition from moved to attack" do
+        @player.move
+        @player.can_attack?.should == true
+      end
     end
-
-    it "should perform a move action" do
-      pending "FSM!"
-    end
-
-    it "should perform an attack action" do
-      pending "FSM!"
-    end
-
   end
-
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require 'simplecov'
-SimpleCov.start
+
+SimpleCov.start do
+  add_filter "/spec/"
+end
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib/dust_tactics'))
