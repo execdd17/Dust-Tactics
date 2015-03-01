fb7152391067fafbf6fd53cbfc40240f49a9f15a
alex <execdd17@gmail.com>
2015-03-01 11:46:37 -0500

    added map texture

--- bin/shoes_game.rb
+++ bin/shoes_game.rb
@@ -5,7 +5,7 @@ $:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 class ShoesGame
   PIECE_WIDTH   = 62
   PIECE_HEIGHT  = 62
-  TOP_OFFSET    = 47
+  TOP_OFFSET    = 15
   LEFT_OFFSET   = 12
 
   BOARD_SIZE    = [8,8]
@@ -27,42 +27,57 @@ class ShoesGame
 
   def draw_board
     @app.clear do
-      @app.background @app.black
-
       @app.stack :margin => 10 do
-        @app.fill @app.rgb(0, 190, 0)
+
+        @app.fill @app.rgb(0, 0, 0, 0.0)
         @app.rect :left => 0, :top => 0, :width => 495, :height => 495
+        @app.background '/home/alex/git_repos/Dust-Tactics/assets/Deep-Freeze.jpg', height: 495
 
         @board.each_with_index do |col, col_index|
           col.each_with_index do |cell, row_index|
             left, top = left_top_corner_of_piece(col_index, row_index)
             left = left - LEFT_OFFSET
             top = top - TOP_OFFSET
-            @app.fill @app.rgb(0, 440, 0, 90)
+            # @app.fill @app.rgb(0, 440, 0, 90)
+            # @app.strokewidth 1
+            @app.stroke @app.rgb(0, 0, 0, 0.25)
             @app.strokewidth 1
-            @app.stroke @app.rgb(0, 100, 0)
+            @app.nofill
             @app.rect :left => left, :top => top, :width => PIECE_WIDTH, :height => PIECE_HEIGHT
 
-            if cell != 0
-              @app.strokewidth 0
-              @app.fill (cell == 1 ? @app.rgb(100,100,100) : @app.rgb(155,155,155))
-              @app.oval(left+3, top+4, PIECE_WIDTH-10, PIECE_HEIGHT-10)
+            # if cell != 0
+              # @app.strokewidth 0
+              # @app.fill (cell == 1 ? @app.rgb(100,100,100) : @app.rgb(155,155,155))
+              # @app.oval(left+3, top+4, PIECE_WIDTH-10, PIECE_HEIGHT-10)
 
-              @app.fill (cell == 1 ? @app.black : @app.white)
-              @app.oval(left+5, top+5, PIECE_WIDTH-10, PIECE_HEIGHT-10)
-            end
+              # @app.fill (cell == 1 ? @app.black : @app.white)
+              # @app.oval(left+5, top+5, PIECE_WIDTH-10, PIECE_HEIGHT-10)
+            # end
           end
         end
+
       end
     end
   end
 
   def left_top_corner_of_piece(a,b)
     [(a*PIECE_WIDTH+LEFT_OFFSET), (b*PIECE_HEIGHT+TOP_OFFSET)]
+    # [(a*PIECE_WIDTH+LEFT_OFFSET), (b*PIECE_HEIGHT)]
   end
 
   def right_bottom_corner_of_piece(a,b)
     left_top_corner_of_piece(a,b).map { |coord| coord + PIECE_WIDTH }
+  end
+
+  def find_piece(x,y)
+    @board.each_with_index { |row_array, row|
+      row_array.each_with_index { |col_array, col|
+        left, top = left_top_corner_of_piece(col, row).map { |i| i - 5}
+        right, bottom = right_bottom_corner_of_piece(col, row).map { |i| i -5 }
+        return [col, row] if x >= left && x <= right && y >= top && y <= bottom
+      }
+    }
+    return false
   end
 end
 
@@ -71,4 +86,12 @@ Shoes.app :width => 520, :height => 600 do
   game = ShoesGame.new(self)
 
   game.draw_board
+
+  click do |button, x, y|
+    if coords = game.find_piece(x,y)
+      puts "you clicked array indexes [#{coords.first}][#{coords.last}]"
+    else
+      puts 'you clicked an invalid space'
+    end
+  end
 end
